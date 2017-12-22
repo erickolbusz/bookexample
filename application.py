81486c0168915e4777325b1e75bf072d8d69091a
erickolbusz <erickolbusz@gmail.com>
2017-12-22 03:46:00 -0500

    trying to fix port binding issue on heroku

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask
+import os
 from bookapp import app
 
 if __name__ == '__main__':
- app.run(debug = True)
+ app.run(debug = True, port=int(os.environ.get('PORT', 5000)))
--- main.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from bookapp import appapp.
-
-if __name__ == '__main__':
- app.run(debug = True)
