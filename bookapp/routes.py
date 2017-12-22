4e3d16e5b029e82b93243ab80c785d171f48aac8
erickolbusz <erickolbusz@gmail.com>
2017-12-22 02:14:30 -0500

    chapters display

--- bookapp/routes.py
+++ bookapp/routes.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
 import re
+import os
 from bs4 import BeautifulSoup
 from bookapp import app
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+BOOK = open(cwd+"/static/data/senseandsensibility.html").read()
 
 def parse_htmlbook(page):
 	links = get_chap_links(page)
@@ -9,6 +13,7 @@ def parse_htmlbook(page):
 	for ind in range(len(links)):
 		section = {}
 		start = links[ind]
+		print ind, len(links)-1
 		if ind < len(links)-1:
 			end = links[ind+1]
 			patt = ('<A NAME="' + start +
@@ -38,6 +43,8 @@ def get_chap_links(page):
 def index():
 	return render_template("index.html")
 
-@app.route('/<chapter>')
-def chapter(chapter):
-	return render_template("base.html")
+@app.route('/<chap>')
+def chapter(chap):
+	(chapters, text) = parse_htmlbook(BOOK)
+	key = 'chap' + ('0'+chap if len(chap)==1 else chap)
+	return render_template("section.html", chapters=chapters, text=text[key])
